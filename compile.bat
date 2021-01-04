5c103ec82637cda0c560e1b0ae889070aede7c3f
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2021-01-04 16:33:28 +0100

    Removed now useless files

--- compile.bat
+++
@@ -1,3 +0,0 @@
-javac .\src\musichub\business\*.java -sourcepath src -d bin -encoding UTF-8 
-javac .\src\musichub\main\*.java -sourcepath src -d bin -encoding UTF-8 
-javac .\src\musichub\util\*.java -sourcepath src -d bin -encoding UTF-8 
