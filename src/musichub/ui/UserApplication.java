b65b2ac09b0a1dc3139fa7fe1f82804ffc4ddd17
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2021-01-04 17:44:00 +0100

    New interface for UserApplication

---
+++ src/musichub/ui/UserApplication.java
@@ -0,0 +1,29 @@
+package musichub.ui;
+
+
+/**
+ *
+ */
+public interface UserApplication {
+
+    /**
+     * Put here everything that should be done on the application's startup
+     */
+    void init();
+
+    /**
+     * Put here everything to run continuously
+     */
+    void update();
+
+    /**
+     * Put here everything that should be done just before the application's end
+     */
+    void end();
+
+    /**
+     * Returns a boolean that indicates if the program has to stop
+     * @return a boolean that indicates if the program has to stop
+     */
+    boolean mustEnd();
+}
